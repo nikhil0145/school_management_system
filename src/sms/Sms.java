94c9f91c2f0184bde9dad4b4015d7f2fe96ff426
NIKHIL KUMAR <40824376+nikhil0145@users.noreply.github.com>
2019-02-27 00:30:40 +0530

    school management system
    A GUI project on school management system using swing

---
+++ src/sms/Sms.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sms;
+
+/**
+ *
+ * @author Bhanu Priya
+ */
+public class Sms {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MainForm objmain=new MainForm();
+        objmain.show();
+    }
+    
+}
